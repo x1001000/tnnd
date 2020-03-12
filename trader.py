5ad619e0a9997777fe089be0cb6b52dae60ee1ed
x1001000 <x1001000@gmail.com>
2020-03-12 19:41:03 +0800

    final

--- trader.py
+++ trader.py
@@ -22,13 +22,13 @@ daytraders = ['陳董', '阿倍']
 daytrading = '1' if user in daytraders else '0'
 
 os.system('cls')
-print(f'{user}您好，我是爽爽弟！\n')
+print(f'{user}您好，我是爽爽！\n')
 month =   input('請問近月代碼: ')
 qty =     input('請問進場幾口: ') #sys.argv[3]
 job = int(input('請問進場幾趟: '))
 print()
-gold = [1001000]*10          #1001000.io
-gold[1]   = int(input('請問觸發通知上車的 近30秒成交量 大於: '))
+gold = [1001000]*10             #1001000.io
+gold[1]   = int(input('請問觸發通知上車的 近60秒成交量 大於: '))
 g,o,l,d   = map(int, input('且做多的 口差 筆差 口差變動 筆差變動 大於: ').split())
 gold[2:6] = g,o,l,d
 g,o,l,d   = map(int, input('或做空的 口差 筆差 口差變動 筆差變動 小於: ').split())
@@ -48,36 +48,36 @@ GOC.AddQuote(broker, prod)
 GOC.AddQuote('Simulator', prod1+','+prod2)
 
 def match2():
-    global volume2, bought2, sold2
+    global Volume2, bought2, sold2
     for tick in GOrder.GOQuote().Describe('Simulator', 'match', prod2):
         try:
-            volume2, bought2, sold2 = map(int, tick[4:])
+            Volume2, bought2, sold2 = map(int, tick[4:])
         except:
             continue
 def commission1():
-    global buying1, selling1
+    global Buying1, Selling1
     for tick in GOrder.GOQuote().Describe('Simulator', 'commission', prod1):
         try:
-            _, buying1, _, selling1 = map(int, tick[2:])
+            buying1, Buying1, selling1, Selling1 = map(int, tick[2:])
         except:
             continue
 def commission2():
-    global buying2, selling2
+    global Buying2, Selling2
     for tick in GOrder.GOQuote().Describe('Simulator', 'commission', prod2):
         try:
-            _, buying2, _, selling2 = map(int, tick[2:])
+            buying2, Buying2, selling2, Selling2 = map(int, tick[2:])
         except:
             continue
 Thread(target=match2).start()
 Thread(target=commission1).start()
 Thread(target=commission2).start()
 
-seq_volume1, seq_b_s, seq_B_S = [], [], []
-def diff(sequence, value, seconds=30):
-    sequence.append((time, value))
-    for t, v in sequence[:]:
+queue = []
+def diff(queue, value, seconds=60):
+    queue.append((time, value))
+    for t, v in queue[:]:
         if parse(time) - parse(t) > timedelta(seconds=seconds):
-            sequence.pop(0)
+            queue.pop(0)
         else:
             return  value - v
 
@@ -87,7 +87,7 @@ def zeroing(clock, minutes):
     while parse(time).time() >= clock:
         minute = clock.minute + minutes
         hour   = clock.hour + minute//60
-        clock  = dtime(hour%24, minute%60)
+        clock  = dtime(hour%24, minute%60, 1)   # 冰火每五分零秒末紀錄
         result =  True
     if minutes == 1:
         clk1 = clock
@@ -109,9 +109,9 @@ def onset():
         int(RODorder)
         GA = GOC.GetAccount(broker, RODorder)
         MA = GOC.MatchAccount(broker, RODorder)
-        LINE(f'委託序號\n{GA[0][:-2]}')
+        LINE(info + f'委託序號\n{GA[0][:-2]}')
     except:
-        LINE(f'錯誤訊息：{RODorder}')
+        LINE(info + f'錯誤訊息：{RODorder}')
 def offset():
     global onboard, todo
     onboard, todo = False, todo-1
@@ -123,17 +123,55 @@ def offset():
             int(IOCorder)
             GA = GOC.GetAccount(broker, IOCorder)
             MA = GOC.MatchAccount(broker, IOCorder)
-            LINE(f'委託序號\n{GA[0][:-2]}')
+            LINE(info + f'委託序號\n{GA[0][:-2]}')
         except:
-            LINE(f'錯誤訊息：{IOCorder}')
+            LINE(info + f'錯誤訊息：{IOCorder}')
 
-print('時間\t', '總量', '量/30s', '口差', '筆差', '口變', '筆變', '價', sep='\t')
-stones2 = stones3 = close = 0
-onboard, todo, first, clk1, clk5, K = False, job, True if user == '團長' else False, dtime(0,0), dtime(0,0), [0,0,0,0,0]
+def plan():
+    global onboard, delay, todo, clk, on, off, price_within, price_to_win, price_to_lose, info
+    if 9 <= parse(time).hour < 12 and not onboard and not delay and todo and clk != clk5:
+        if stones[1] > gold[1] and (
+            stones[2] > gold[2] and stones[3] > gold[3] and stones[4]+stonez[4] > gold[4] and stones[5]+stonez[5] > gold[5] and 0 < stonez[4] < 900 and 0 < stonez[5] < 700 and K[3] > K[2] and K[1] >= K[0] or
+            stones[2] < gold[6] and stones[3] < gold[7] and stones[4]+stonez[4] < gold[8] and stones[5]+stonez[5] < gold[9] and 0 > stonez[4] >-900 and 0 > stonez[5] >-700 and K[3] < K[2] and K[1] <= K[0] ):
+            onboard, clk = True, clk5
+            s_p, s_l = (p, l) if abs(K[3] - K[2]) < abs(K[1] - K[0]) else (P, L)
+            on, off = ('B', 'S') if stonez[5] > 0 else ('S', 'B')
+            price_within  = price + (rod if on == 'B' else -rod)
+            price_to_win  = price + (s_p if on == 'B' else -s_p)
+            price_to_lose = price - (s_l if on == 'B' else -s_l)
+            info += f"上車做{'多' if on == 'B' else '空'}。。。\n\n"
+            onset()
+    elif onboard:
+        if 13 <= parse(time).hour < 14:
+            onboard, delay, todo = False, False, job
+            info += '被老司機趕下車了\n\n'
+            offset()
+        elif on == 'B' and price <= price_to_lose or \
+             on == 'S' and price >= price_to_lose:
+            onboard, delay, todo = False, False, todo-1
+            info += '下車停損 (╥﹏╥)\n\n'
+            offset()
+        elif on == 'B' and price >= price_to_win or \
+             on == 'S' and price <= price_to_win:
+            onboard, delay, todo = False, price_to_win, todo-1
+    elif delay:
+        if on == 'B' and price > price_to_win or \
+           on == 'S' and price < price_to_win:
+            price_to_win = delay + (1 if on == 'B' else -1)
+        elif on == 'B' and price < price_to_win or \
+             on == 'S' and price > price_to_win:
+            delay = False
+            info += '下車停利 (*´∀`)~♥\n\n'
+            offset()
+    elif not 8 <= parse(time).hour < 14:
+        onboard, delay, todo = False, False, job
 
+onboard, delay, todo, clk, clk1, clk5, K, close, stonez = False, False, job, dtime(), dtime(), dtime(), [0]*5, 0, [0]*6
+
+print('時間\t', '總量', '量/60s', '口差', '筆差', '口變', '筆變', '價', sep='\t')
 for tick in GOrder.GOQuote().Describe('Simulator', 'match', prod1):
     try:
-        time, price, lots, volume1, bought1, sold1 = tick[0], *map(int, tick[2:])
+        time, price, Lots, Volume1, bought1, sold1 = tick[0], *map(int, tick[2:])
     except:
         continue
     try:
@@ -141,58 +179,32 @@ for tick in GOrder.GOQuote().Describe('Simulator', 'match', prod1):
     except:
         continue
     try:
-        buying1
+        Buying1
     except:
         continue
     try:
-        buying2
+        Buying2
     except:
         continue
 
-    stones = [
-        volume1 + volume2,
-        diff(seq_volume1, volume1),
-        buying1 - selling1 + buying2 - selling2,
-        sold1 - bought1 + sold2 - bought2,
-        buying1 - selling1 + buying2 - selling2 - stones2,
-        sold1 - bought1 + sold2 - bought2       - stones3]
+    stones = [Volume1 + Volume2]
+    stones.append(diff(queue, Volume1))
+    stones.append(Buying1 - Selling1 + Buying2 - Selling2)
+    stones.append(sold1 - bought1 + sold2 - bought2)
+    stones.append(stones[2] - stonez[2])
+    stones.append(stones[3] - stonez[3])
     if zeroing(clk1, 1):
         K.pop(0)
         K.pop(0)
         K.append(close)
         K.append(price)
+        if delay:
+            delay = price_to_win = price
     close = price
     if zeroing(clk5, 5):
-        stones2, stones3, stones[4], stones[5] = stones[2], stones[3], 0, 0
-    print(time.split()[-1], *stones, price, sep='\t')
-    info = time + '\n' + str(stones[1:]) + '\n' + str(K[:-1]) + '\n' + str(price) + '\n' + user
+        stonez = stones[:]
+        stones[4:] = 0, 0
 
-    if 8 <= parse(time).hour < 13 and first and stones[1] > 900:
-        first = False
-        LINE(info[:-2]+'今日首班車來囉！')
-    
-    if 9 <= parse(time).hour < 12 and not onboard and todo:
-        if stones[1] > gold[1]:
-            if  stones[2] > gold[2] and stones[3] > gold[3] and stones[4] > gold[4] and stones[5] > gold[5] and K[3] > K[2] and K[1] >= K[0] or \
-                stones[2] < gold[6] and stones[3] < gold[7] and stones[4] < gold[8] and stones[5] < gold[9] and K[3] < K[2] and K[1] <= K[0]:
-                s_p, s_l = (p, l) if abs(K[3] - K[2]) < abs(K[1] - K[0]) else (P, L)
-                on, off = ('B', 'S') if stones[5] > 0 else ('S', 'B')
-                price_within  = price + (rod if on == 'B' else -rod)
-                price_to_win  = price + (s_p if on == 'B' else -s_p)
-                price_to_lose = price - (s_l if on == 'B' else -s_l)
-                LINE(info+'上車做'+('多' if on == 'B' else '空')+'。。。')
-                onset()
-    elif onboard:
-        if 13 <= parse(time).hour < 14:
-            LINE(info+'被老司機趕下車了')
-            offset()
-        elif on == 'B' and price >= price_to_win or \
-            on == 'S' and price <= price_to_win:
-            LINE(info+'下車停利 (*´∀`)~♥')
-            offset()
-        elif on == 'B' and price <= price_to_lose or \
-            on == 'S' and price >= price_to_lose:
-            LINE(info+'下車停損 (╥﹏╥)')
-            offset()
-    elif not 8 <= parse(time).hour < 13:
-        todo, first = job, True if user == '團長' else False
+    print(time.split()[-1], *stones, price, sep='\t')
+    info = f'{time}\n{stones[1:]}\n{stonez[1:]}\n{K[:-1]}\n{price}\n{user}'
+    plan()
